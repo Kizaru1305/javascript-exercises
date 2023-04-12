a5251eebd5fb596fb2577862ad44c15e97530f51
Softo <ghernandez1305@gmail.com>
2023-04-11 21:52:50 -0400

    Finished 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    const word = new Array(num);
+    for(let i = 0; i < num; i++) {
+        word[i] = string;
+    }
+    return word.join("");
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let letters = string.split("");
+    let reversedStr = '';
+    for(let i = string.length - 1; i >= 0; i--){
+        reversedStr += letters[i];
+    }
+    return reversedStr;   
 };
 
 // Do not edit below this line
